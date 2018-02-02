93bb1fad1849d52e1955a9ca3516b1dda1932321
Langlee Threlkeld <langleet@gmail.com>
2018-02-01 20:27:38 -0600

    Initial Commit 2/1/2018

---
+++ org-charts/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { IEmployee } from 'ng-org-chart';
+
+@Component({
+	selector: 'app-core',
+	templateUrl: './app.component.html'
+})
+export class AppComponent {
+}
